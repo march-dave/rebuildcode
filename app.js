d4eaf6516a9786694fabbdc75d7583cdb0597ce1
march-dave <ysangy@gmail.com>
2016-05-30 01:12:08 -0700

    final example

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
 var app = express();
 
-var routes = require('./routes');
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.set('views', __dirname + '/views');
 
-app.locals.pagetitle = "Awesome Website";
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', routes.index);
-app.get('/about', routes.about);
+app.locals.appdata = require('./data.json');
+app.use('/', routes);
 
-app.get('*', function(req, res) {
-  res.send("Bad Route");
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-// var http = require('http');
-//
-// var myServer = http.createServer(function(request, response) {
-//     response.writeHead(200, { "Content-Type": "text/html"} );
-//     response.write("<b>Hello</b> World");
-//     response.end();
-// });
-//
-// myServer.listen(3000);
-//
-// console.log("Go to http://localhost:3000 on your browser");
+
+module.exports = app;
